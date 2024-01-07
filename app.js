aae9d9d8f0583d6d7d834c71e447257bc375215a
Kunal Rathod <kunalrathod11131@gmail.com>
2024-01-07 14:15:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,15 +18,15 @@ const localStrategy = require("passport-local");
 const User = require("./models/user.js");
 
 
-app.get("/", (req, res)=>{
-    res.send("Hi , I am root");
-});
+// app.get("/", (req, res)=>{
+//     res.send("Hi , I am root");
+// });
 
 const listingRouter = require("./routes/listings.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
   
 const dbUrl = process.env.ATLASDB_URL;
 async function main(){
